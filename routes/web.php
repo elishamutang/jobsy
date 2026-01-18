861367a8ed3ac9455aec79d26f988520d605aa7b
elishamutang <elishamutang@outlook.com>
2026-01-18 21:45:12 +0800

    update: Added initial routes for job listings.

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,19 @@
 <?php
 
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/login', function () {
 //     return inertia('Login');
 // });
 
+// Authenticated Home Page
 Route::get('/', function () {
     return inertia('Home');
+});
+
+// Show all jobs
+Route::prefix('jobs', function () {
+    Route::get('/{job}', [JobController::class, 'show']);
+    Route::get('/', [JobController::class, 'index']);
 });
