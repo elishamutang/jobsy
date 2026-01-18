0cfdf95c08d33c3987980b72b79c5c4f0ba375e2
elishamutang <elishamutang@outlook.com>
2026-01-18 21:44:45 +0800

    update: Added intial methods.

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -6,8 +6,14 @@ use Illuminate\Http\Request;
 
 class JobController extends Controller
 {
-    // Return all listings
+    // Return all jobs
     public function index()
+    {
+        //
+    }
+
+    // Return a specific job
+    public function show()
     {
         //
     }
