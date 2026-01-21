a3eb29650cf09bc38dcb3e2e583e1d242ad11a58
elishamutang <elishamutang@outlook.com>
2026-01-22 00:03:39 +0800

    update: Updated Layout component..

--- resources/js/Layouts/Layout.jsx
+++ resources/js/Layouts/Layout.jsx
@@ -1,11 +1,15 @@
+import { Link } from "@inertiajs/react";
+
 export default function Layout({ children }) {
     return (
         <>
-            <main className="mx-auto my-10 flex justify-center flex-col items-center md:w-1/2 md:border rounded-md p-5">
+            <main className="mx-auto my-3 flex justify-center flex-col items-center md:w-1/2 md:border rounded-md p-5">
                 <section>
-                    <h1 className="text-5xl md:text-7xl font-bold mb-10 font-helvetica tracking-tight">
-                        <span className="text-7xl">J</span>obsy
-                        <span className="text-blue-600">.</span>
+                    <h1 className="text-5xl md:text-6xl font-bold mb-5 font-helvetica tracking-tight">
+                        <Link href="/">
+                            <span className="text-7xl md:text-8xl">J</span>obsy
+                            <span className="text-blue-600">.</span>
+                        </Link>
                     </h1>
                 </section>
                 {children}
