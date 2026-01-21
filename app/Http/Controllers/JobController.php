51cc4a2933b927f03030ae70b09091195fcfe06c
elishamutang <elishamutang@outlook.com>
2026-01-22 00:02:40 +0800

    update: Added create method.

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -2,19 +2,41 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Job;
+use Inertia\Inertia;
 
 class JobController extends Controller
 {
     // Return all jobs
     public function index()
     {
-        //
+        $jobs = Job::paginate(10);
+
+        // Modify the "Previous" and "Next" button text
+        $paginatedData = $jobs->toArray();
+
+        $lastIdx = count($paginatedData['links']) - 1;
+
+        $paginatedData['links'][0]['label'] = 'Previous';
+        $paginatedData['links'][$lastIdx]['label'] = 'Next';
+
+        // Get all jobs
+        return Inertia::render('Home', [
+            'jobs' => $paginatedData,
+        ]);
     }
 
     // Return a specific job
-    public function show()
+    public function show(Job $job)
     {
-        //
+        return Inertia::render('Jobs/Show', [
+            'job' => $job,
+        ]);
+    }
+
+    // Render job creation page
+    public function create()
+    {
+        return Inertia::render('Jobs/Create');
     }
 }
