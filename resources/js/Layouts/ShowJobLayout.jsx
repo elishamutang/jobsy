047de24bdbd8ee947288a4776bd89d607917ac35
elishamutang <elishamutang@outlook.com>
2026-01-22 23:46:57 +0800

    update: Added default layout for ShowJob page.

---
+++ resources/js/Layouts/ShowJobLayout.jsx
@@ -0,0 +1,27 @@
+import { Link } from "@inertiajs/react";
+
+export default function ShowJobLayout({ children }) {
+    return (
+        <>
+            <main className="mx-auto my-3 flex justify-center flex-col items-center md:w-1/2 md:border rounded-md p-5">
+                <section className="self-start flex justify-between items-center w-full">
+                    <h1 className="text-2xl md:text-6xl font-bold font-helvetica tracking-tight">
+                        <Link href="/">
+                            <span className="text-5xl md:text-8xl">J</span>obsy
+                            <span className="text-blue-600">.</span>
+                        </Link>
+                    </h1>
+
+                    {/* Edit Button */}
+                    <button className="self-end md:text-xl md:px-6 btn bg-blue-800">
+                        Edit
+                    </button>
+                </section>
+
+                <div className="divider my-1"></div>
+
+                {children}
+            </main>
+        </>
+    );
+}
--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -4,12 +4,19 @@ import { createRoot } from "react-dom/client";
 import { renderApp } from "@inertiaui/modal-react";
 import Layout from "./Layouts/Layout";
 import "../css/app.css";
+import ShowJobLayout from "./Layouts/ShowJobLayout";
 
 createInertiaApp({
     resolve: (name) => {
         const pages = import.meta.glob("./Pages/**/*.jsx", { eager: true });
         let page = pages[`./Pages/${name}.jsx`];
-        page.default.layout = (page) => <Layout children={page} />;
+
+        if (name !== "Home") {
+            page.default.layout = (page) => <ShowJobLayout children={page} />;
+        } else if (!page.default.layout) {
+            page.default.layout = (page) => <Layout children={page} />;
+        }
+
         return page;
     },
     setup({ el, App, props }) {
