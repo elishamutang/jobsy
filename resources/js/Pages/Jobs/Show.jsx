456e1c511161fc60c80355449955bf1a32cc3d2d
elishamutang <elishamutang@outlook.com>
2026-01-22 23:46:40 +0800

    update: Added show job page.

--- resources/js/Pages/Jobs/Show.jsx
+++ resources/js/Pages/Jobs/Show.jsx
@@ -2,7 +2,100 @@ export default function Show({ job }) {
     console.log(job);
     return (
         <>
-            <h1 className="">{job.title}</h1>
+            <section className="self-start w-full flex flex-col gap-5">
+                {/* Top section */}
+                <div className="flex">
+                    {/* Job title */}
+                    <div className="flex flex-col w-full justify-between">
+                        <h1 className="font-semibold w-3/4 font-helvetica text-3xl">
+                            {job.title}
+                        </h1>
+
+                        {/* Company */}
+                        <p className="font-helvetica text-gray-400">
+                            {/* FUTURE: This can be a link to the company */}
+                            at{" "}
+                            <span className="hover:border-b cursor-pointer ease-in-out transition-all">
+                                {job.company}
+                            </span>
+                            <span>, {job.type}.</span>
+                        </p>
+                    </div>
+
+                    {/* Job ID */}
+                    <div className="md:border md:border-5 md:px-4 md:rounded-full flex">
+                        <p className="font-helvetica text-5xl self-start md:text-3xl md:self-center font-bold">
+                            1
+                        </p>
+                    </div>
+                </div>
+
+                {/* Industry */}
+                <div>
+                    <h2 className="font-helvetica font-semibold text-2xl">
+                        Industry
+                    </h2>
+                    <p className="text-gray-400 font-helvetica">
+                        {job.industry}
+                    </p>
+                </div>
+
+                {/* Location */}
+                <div>
+                    <h2 className="font-helvetica font-semibold text-2xl">
+                        Location
+                    </h2>
+                    <p className="text-gray-400 font-helvetica">
+                        {job.location}{" "}
+                        <span className="font-helvetica text-gray-500">
+                            - {job.location_type}
+                        </span>
+                    </p>
+                </div>
+
+                {/* Status of applied job */}
+                <div>
+                    <h2 className="font-helvetica font-semibold text-2xl">
+                        Status
+                    </h2>
+                    <p
+                        className={`font-helvetica tracking-wide text-gray-400 ${job.status === "Offer" ? "text-green-400" : job.status === "Rejected" ? "text-red-500" : job.status === "Pending" ? "text-orange-500" : ""}`}
+                    >
+                        {job.status}
+                    </p>
+                </div>
+
+                {/* Date applied and closing date */}
+                <div>
+                    <h2 className="font-helvetica font-semibold text-2xl">
+                        Dates
+                    </h2>
+                    <div>
+                        <p className="font-helvetica text-gray-500">
+                            You applied on{" "}
+                            <span className="border-b border-b-green-500">
+                                {job.date_applied}
+                            </span>
+                        </p>
+                        <p className="font-helvetica text-gray-500">
+                            This posting closes on{" "}
+                            <span className="border-b border-b">
+                                {job.closing_date}
+                            </span>
+                        </p>
+                    </div>
+                </div>
+
+                {/* Last updated */}
+                <div className="mt-5 md:flex-row md:justify-between flex flex-col justify-between w-full items-center">
+                    <p className="font-helvetica text-sm text-gray-600">
+                        Created on <span>{job.created_at}</span>
+                    </p>
+                    <p className="font-helvetica text-sm text-gray-600">
+                        Last updated on <span>{job.updated_at}</span>
+                    </p>
+                </div>
+            </section>
         </>
     );
 }
