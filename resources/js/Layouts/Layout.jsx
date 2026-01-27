6d11d0c67b8428b2d69259fc2ac94eb745f49a26
elishamutang <elishamutang@outlook.com>
2026-01-27 16:09:06 +0800

    update: Updated layout styling.

--- resources/js/Layouts/Layout.jsx
+++ resources/js/Layouts/Layout.jsx
@@ -3,7 +3,7 @@ import { Link } from "@inertiajs/react";
 export default function Layout({ children }) {
     return (
         <>
-            <main className="mx-auto my-3 flex justify-center flex-col items-center md:w-1/2 md:border rounded-md p-5">
+            <main className="mx-auto my-3 flex justify-center flex-col items-center w-full sm:w-xl md:w-2xl 2xl:border md:border-zinc-600 md:border-2 rounded-md pt-5 px-5 md:px-10">
                 <section>
                     <h1 className="text-5xl md:text-6xl font-bold mb-5 font-helvetica tracking-tight">
                         <Link href="/">
