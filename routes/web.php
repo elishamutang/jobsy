87687bab4b305503039028b706febd85e7975d26
elishamutang <elishamutang@outlook.com>
2026-01-29 00:02:08 +0800

    update: Updated routes.

--- routes/web.php
+++ routes/web.php
@@ -3,19 +3,29 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
-// Authentication routes
+// Authenticated Home Page
+Route::get('/', function () {
+    return Inertia::render('Home');
+})->name('index')->middleware('guest');
+
+// User Registration
 Route::get('/register', [AuthController::class, 'register']);
-Route::get('/login', [AuthController::class, 'index']);
-Route::get('/logout', [AuthController::class, 'logout'])->middleware('auth');
+Route::post('/register', [AuthController::class, 'store']);
 
+// User Login & Logout
+Route::get('/login', [AuthController::class, 'index'])->middleware('guest');
+Route::post('/login', [AuthController::class, 'login'])->middleware('guest');
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth');
 
-// Show job
+// To be protected routes
 Route::prefix('jobs')->group(function () {
     Route::get('/create', [JobController::class, 'create']);
     Route::post('/create', [JobController::class, 'store']);
-    Route::get('/{job}', [JobController::class, 'show']);
-});
 
-// Authenticated Home Page
-Route::get('/', [JobController::class, 'index']);
+    Route::get('/{job}', [JobController::class, 'show']);
+
+    // Authenticated home page
+    Route::get('/', [JobController::class, 'index'])->name('home');
+});
