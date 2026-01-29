d590c55eed08cf0782a66d442e38c69a9826b664
elishamutang <elishamutang@outlook.com>
2026-01-29 23:44:22 +0800

    update: Removed Search button.

--- resources/js/Components/Actions.jsx
+++ resources/js/Components/Actions.jsx
@@ -3,13 +3,16 @@ import { Form } from "@inertiajs/react";
 export default function Actions() {
     return (
         <>
-            <Form action="/jobs" method="post" className="flex gap-2 py-2 px-5">
+            <Form
+                action="/jobs"
+                method="post"
+                className="py-2 flex justify-center w-full"
+            >
                 <input
                     type="text"
                     placeholder="Search job here"
                     className="input"
                 />
-                <button className="btn bg-slate-600">Search</button>
             </Form>
         </>
     );
