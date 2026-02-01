25c4dbe2d57a08fd23048e9f1060e351cafa5288
elishamutang <elishamutang@outlook.com>
2026-02-01 17:14:54 +0800

    update: Added a back to login button.

--- resources/js/Pages/Register.jsx
+++ resources/js/Pages/Register.jsx
@@ -1,60 +1,66 @@
-import { Form } from "@inertiajs/react";
+import { Form, Link } from "@inertiajs/react";
 
 export default function Register() {
     return (
         <>
             {/* Registration form */}
-            <Form
-                action="/register"
-                method="post"
-                className="flex flex-col items-center w-8/9"
-            >
-                <div className="w-full flex flex-col my-2">
-                    {/* User's name */}
-                    <label htmlFor="name">Name</label>
-                    <input
-                        type="text"
-                        name="name"
-                        id="name"
-                        className="input w-full mt-2 mb-3"
-                    />
+            <section className="w-8/9 flex flex-col gap-2">
+                <Form action="/register" method="post">
+                    <div className="w-full flex flex-col my-2">
+                        {/* User's name */}
+                        <label htmlFor="name">Name</label>
+                        <input
+                            type="text"
+                            name="name"
+                            id="name"
+                            className="input w-full mt-2 mb-3"
+                        />
 
-                    {/* User email */}
-                    <label htmlFor="email">Email</label>
-                    <input
-                        type="email"
-                        name="email"
-                        id="email"
-                        className="input w-full mt-2 mb-3"
-                    />
+                        {/* User email */}
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            name="email"
+                            id="email"
+                            className="input w-full mt-2 mb-3"
+                        />
 
-                    {/* User password */}
-                    <label htmlFor="password">Password</label>
-                    <input
-                        type="password"
-                        name="password"
-                        id="password"
-                        className="input w-full mt-2 mb-3"
-                    />
+                        {/* User password */}
+                        <label htmlFor="password">Password</label>
+                        <input
+                            type="password"
+                            name="password"
+                            id="password"
+                            className="input w-full mt-2 mb-3"
+                        />
 
-                    {/* Confirm password */}
-                    <label htmlFor="password_confirmation">
-                        Confirm Password
-                    </label>
-                    <input
-                        type="password"
-                        name="password_confirmation"
-                        id="password_confirmation"
-                        className="input w-full mt-2 mb-3"
-                    />
-                </div>
-                <button
-                    type="submit"
-                    className="btn w-full bg-blue-900 text-lg py-6"
+                        {/* Confirm password */}
+                        <label htmlFor="password_confirmation">
+                            Confirm Password
+                        </label>
+                        <input
+                            type="password"
+                            name="password_confirmation"
+                            id="password_confirmation"
+                            className="input w-full mt-2 mb-3"
+                        />
+                    </div>
+                    <button
+                        type="submit"
+                        className="btn w-full bg-blue-900 text-lg py-6"
+                    >
+                        Create
+                    </button>
+                </Form>
+
+                {/* Back to login page */}
+                <Link
+                    href="/login"
+                    className="btn bg-blue-500 font-helvetica font-semibold tracking-wide"
                 >
-                    Create
-                </button>
-            </Form>
+                    Already have an account? Login.
+                </Link>
+            </section>
         </>
     );
 }
