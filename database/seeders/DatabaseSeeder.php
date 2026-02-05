b99148d85776773cf66f73b1a3ff2cd1d41d360f
elishamutang <elishamutang@outlook.com>
2026-02-05 19:58:15 +0800

    update: Re-factor main seeder file.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,18 +16,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $user = User::factory([
+        User::factory()->create([
             'name' => 'Elisha',
             'email' => 'elishamutang@outlook.com',
             'password' => 1234,
         ]);
 
-        // Seed countries first
+        // Run additional seeders
         $this->call([
             CountrySeeder::class,
+            JobSeeder::class,
         ]);
-
-        // Create 20 random jobs for the above user
-        Job::factory(20)->for($user)->create();
     }
 }
