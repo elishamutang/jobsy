7927fc7f124f831a03daf3dc148c9659d25c53de
elishamutang <elishamutang@outlook.com>
2026-02-07 18:20:25 +0800

    update: Added email tests.

---
+++ tests/Feature/Auth/EmailVerificationTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
+use Tests\TestCase;
+
+class EmailVerificationTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * Render email verification notice after successful registration.
+     */
+    public function test_redirect_user_to_email_verification_page_if_email_is_unverified(): void
+    {
+        // Arrange data
+        $user = [
+            'name' => 'Example User',
+            'email' => 'user@example.com',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ];
+
+        // Act
+        $response = $this->post('/register', $user);
+
+        // Assert
+        $response->assertStatus(302);
+        $response->assertRedirectToRoute('verification.notice');
+    }
+}
