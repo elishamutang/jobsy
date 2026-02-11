d624585544bc6f5d88b28fe578e65f4710f9d056
elishamutang <elishamutang@outlook.com>
2026-02-11 23:46:33 +0800

    update: Added JobLevel table and foreign constraint on user_jobs table.

---
+++ database/migrations/2026_02_11_154223_add_foreign_key_to_user_jobs_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('user_jobs', function (Blueprint $table) {
+            $table->unsignedBigInteger('job_level')->change();
+            $table->foreign('job_level')->references('id')->on('job_level');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('user_jobs', function (Blueprint $table) {
+            $table->dropForeign('job_level');
+        });
+    }
+};
