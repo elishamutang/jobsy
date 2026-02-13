fdad01c2ca68088490a9f53ea28fd444ed581fbc
elishamutang <elishamutang@outlook.com>
2026-02-14 01:32:11 +0800

    update: Added country column to users table.

---
+++ database/migrations/2026_02_13_164512_add_country_column_to_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('country')->nullable()->default(null);
+            $table->foreign('country')->references('id')->on('countries');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('country');
+            $table->dropForeign('country');
+        });
+    }
+};
