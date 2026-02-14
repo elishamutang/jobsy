19080ea1ef9c606b3f140f6fc8e36df2fee455f3
elishamutang <elishamutang@outlook.com>
2026-02-14 16:09:16 +0800

    update: Initialized FontAwesome.

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -8,6 +8,12 @@ import ShowJobLayout from "./Layouts/ShowJobLayout";
 import CreateJobLayout from "./Layouts/CreateJobLayout";
 import LoginOrRegisterLayout from "./Layouts/LoginOrRegisterLayout";
 import EditJobLayout from "./Layouts/EditJobLayout";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+
+library.add(fas, far, fab);
 
 createInertiaApp({
     resolve: (name) => {
