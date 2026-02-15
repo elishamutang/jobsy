50b28fdcc04df79666ba2b28845ae1d609cc3d61
elishamutang <elishamutang@outlook.com>
2026-02-15 22:59:19 +0800

    update: Added twMerge function for css class override.

--- resources/js/Components/LightOrDarkModeToggle.jsx
+++ resources/js/Components/LightOrDarkModeToggle.jsx
@@ -1,15 +1,19 @@
 import { useContext } from "react";
 import { ThemeContext } from "../Hooks/useTheme";
 import { SunIcon, MoonIcon } from "@heroicons/react/24/outline";
+import { twMerge } from "tailwind-merge";
 
-export default function LightOrDarkModeToggle() {
+export default function LightOrDarkModeToggle({ className }) {
     const { theme, toggleTheme } = useContext(ThemeContext);
 
     return (
         <>
             <button
                 onClick={toggleTheme}
-                className="self-start absolute shadow-md py-2 px-3 rounded-lg btn cursor-pointer transition-all dark:bg-gray-800 dark:border-none dark:hover:bg-gray-700"
+                className={twMerge(
+                    "self-start shadow-md py-2 px-3 rounded-lg btn cursor-pointer transition-all dark:bg-gray-800 dark:border-none dark:hover:bg-gray-700",
+                    className,
+                )}
             >
                 {theme === "dark" ? (
                     <SunIcon className="size-6 text-gray-300 hover:fill-yellow-500" />
