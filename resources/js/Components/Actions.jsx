5f9babc6072c7cedc7cae512749b62365b49eb34
elishamutang <elishamutang@outlook.com>
2026-02-15 16:56:38 +0800

    update: Completed search feature with debouncing.

--- resources/js/Components/Actions.jsx
+++ resources/js/Components/Actions.jsx
@@ -1,19 +1,53 @@
 import { Form } from "@inertiajs/react";
+import { useRef, useState } from "react";
 
-export default function Actions() {
+export default function Actions({ value, onChangeHandler }) {
+    const [isFocused, setIsFocused] = useState(false);
+
+    const inputRef = useRef(null);
+
+    // Focus input when clicked
+    const handleSpanClick = () => {
+        setIsFocused(true);
+        inputRef.current?.focus();
+    };
+
     return (
-        <>
-            {/* TODO(elishamutang): Add method and action attributes */}
-            <Form className="py-2 flex gap-2 w-full">
+        <Form className="py-2 flex gap-2 w-full">
+            <div className="relative w-full">
                 <input
+                    ref={inputRef}
                     type="text"
-                    placeholder="Search job here"
-                    className="input flex-1"
+                    name="search"
+                    id="search"
+                    placeholder=""
+                    className="input flex-1 w-full"
+                    value={value}
+                    onChange={(e) => onChangeHandler(e.target.value)}
+                    onFocus={() => setIsFocused(true)}
+                    onBlur={() => setIsFocused(false)}
                 />
-                <button className="btn dark:bg-slate-800 dark:text-black dark:text-white">
-                    Search
-                </button>
-            </Form>
-        </>
+                {!isFocused && value.length === 0 && (
+                    <span
+                        onClick={handleSpanClick}
+                        className={`absolute left-3 top-2.5 text-sm text-gray-400`}
+                    >
+                        Search by
+                        <span className="ml-1 text-rotate">
+                            <span>
+                                <span>job title</span>
+                                <span>job type</span>
+                                <span>job industry</span>
+                                <span>company</span>
+                                <span>country</span>
+                            </span>
+                        </span>
+                    </span>
+                )}
+            </div>
+            <button className="btn dark:bg-slate-800 dark:text-black dark:text-white">
+                Search
+            </button>
+        </Form>
     );
 }
